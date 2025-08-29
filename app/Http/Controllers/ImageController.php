477e8ae1340f2feaf994062d9b65cb8aac07f6dc
daffabot <daffabot@programmer.net>
2025-08-29 10:34:12 +0700

    kelupaan nambah Str

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -7,6 +7,7 @@ use App\Models\Image;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class ImageController extends Controller
 {
--- app/Http/Controllers/MusicController.php
+++ app/Http/Controllers/MusicController.php
@@ -7,6 +7,7 @@ use App\Models\Music;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class MusicController extends Controller
 {
